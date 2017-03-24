57d744772eb56568332f00ff258c4d3fd1b61eea
Vivek Khurana <vkhurana@bebotechnologies.com>
2017-03-24 15:39:38 +0530

    Complete select all method

--- ruby/db_methods/sql_methods.rb
+++ ruby/db_methods/sql_methods.rb
@@ -1,3 +1,8 @@
+require "mysql2"
+require "pry"
+
+$db_client = Mysql2::Client.new(:host => "localhost", :username => "root", :database=> "utms")
+
 def resolve_where_clause(where_clause=[], where_condition = 'AND')
 	where_string='', index = 0
 	where_clause.each do |where|
@@ -12,10 +17,42 @@ def resolve_where_clause(where_clause=[], where_condition = 'AND')
 end
 
 def select_all(table_name,where_clause=[],where_condition='AND')
-	query = "SELECT * FROM `#[table_name}`"
-	$where_string = '';
+	db_query = "SELECT * FROM `#{table_name}`"
+	where_string = '';
 	if where_clause!=[]
 		where_string = resolve_where_clause(where_clause, where_condition)
-		query = "#{query} WHERE #{where_string}"
+		db_query = "#{db_query} WHERE #{where_string}"
 	end
+	result_array = [];
+	$db_client.query(db_query).each do |row|
+		result_array << row
+	end
+	result_array
 end
+
+def select_fields(fields=[],table_name,where_clause=[],where_condition='AND'){
+	select_string = "";
+	# TODO: Manipulate method from here
+	for($i=0;$i<sizeof($fields);$i++){
+		if($i<sizeof($fields) && $i!=(sizeof($fields)-1)){
+			$select_string = $select_string."`".$fields[$i]."`,";
+		}
+		elseif($i>=(sizeof($fields)-1)){
+			$select_string = $select_string."`".$fields[$i]."`";
+		}
+	}
+	$query = "SELECT ".$select_string." FROM `".$table_name."`";
+	$where_string = '';
+	if($where_clause!=[]){
+		$where_string = resolve_where_clause($where_clause, $where_condition);
+		$query = $query." WHERE ".$where_string;
+	}
+	$result = mysql_query($query) or DIE(mysql_error());
+	$result_array = [];
+	while($row = mysql_fetch_array($result)){
+		array_push($result_array, $row);
+	}
+	return $result_array;
+}
+
+puts select_all('login_details',['primary_contact','9569793959'])
